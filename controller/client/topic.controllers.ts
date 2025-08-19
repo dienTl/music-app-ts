6d1a192207f1160838b49a3d458a0f68760a8ddd
buiminhdien <dienbui2005@gmail.com>
2025-08-19 22:03:12 +0700

    cấu trúc lại pug

--- controller/client/topic.controllers.ts
+++ controller/client/topic.controllers.ts
@@ -6,5 +6,7 @@ export const topics = async (Request :Request , res : Response) =>{
   const topics = await Topic.find({
     deleted :false
   })
-  res.render("client/pages/topics/index.pug")
+  res.render("client/pages/topics/index.pug",{
+    pagetitle :"Chủ đề bài hát"
+  })
 }
