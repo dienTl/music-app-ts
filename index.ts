53dd8b1e386e128138a00952d2d1bb99ede6dcb2
buiminhdien <dienbui2005@gmail.com>
2025-08-19 16:02:57 +0700

    tạo route cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("ok")
+import express ,{ Express, Request ,Response } from "express";
+
+const app : Express = express();
+const port :number = 3000 ;
+
+app.get("/topics",(req:Request ,res : Response) =>{
+  res.send("chủ đề bài hát")
+});
+
+app.listen(port ,()=>{
+  console.log(`app listening on port ${port}`)
+})
