1e2b66439c276d3e44d3be44ebcd435247983153
buiminhdien <dienbui2005@gmail.com>
2025-08-20 09:27:07 +0700

    làm trang cá nhân

--- controller/client/topic.controllers.ts
+++ controller/client/topic.controllers.ts
@@ -4,9 +4,10 @@ import Topic from "../../models/topics.model"
 //[GET] /topics/
 export const topics = async (Request :Request , res : Response) =>{
   const topics = await Topic.find({
-    deleted :false
+    deleted:false
   })
   res.render("client/pages/topics/index.pug",{
-    pagetitle :"Chủ đề bài hát"
+    pagetitle :"Chủ đề bài hát",
+    topics : topics
   })
 }
