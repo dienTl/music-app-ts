8dc117ca61f0e0b53a1f98d7781633d3f84409d2
buiminhdien <dienbui2005@gmail.com>
2025-08-23 13:31:44 +0700

    previw ảnh trong create

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -4,20 +4,41 @@
     const uploadImageInput = document.querySelector("[upload-image-input]")
     const uploadImagePreview = document.querySelector("[upload-image-preview]")
     uploadImageInput.addEventListener("change" , (e) =>{
-      const file = e.target.files[0]
-      uploadImagePreview.src = URL.createObjectURL(file)
+      if(e.target.files.length){
+      const image = URL.createObjectURL(e.target.files[0])
+      uploadImagePreview.src = image
+      }
     })
   }
 
-  const HUY = document.querySelector("[HUY]");
-if (HUY) {
-  HUY.addEventListener("click", () => {
-    const uploadImageInput = document.querySelector("[upload-image-input]");
-    const uploadImagePreview = document.querySelector("[upload-image-preview]");
-    if (uploadImageInput && uploadImagePreview) {
-      uploadImageInput.value = "";
-      uploadImagePreview.src = "";
-    }
-  });
-}
 //end upload image
+
+// upload audio
+  const uploadAudio = document.querySelector("[upload-audio]")
+  if(uploadAudio){
+    const uploadAudioInput = document.querySelector("[upload-audio-input]")
+    const uploadAudioPlay = document.querySelector("[upload-image-play]")
+    const source = uploadAudio.querySelector("source")
+    uploadAudioInput.addEventListener("change" , (e) =>{
+      if(e.target.files.length){
+        const audio = URL.createObjectURL(e.target.file[0])
+        source.scr = audio
+        uploadAudio.load()
+      }
+
+    })
+  }
+
+  // const HUY = document.querySelector("[HUY]");
+// if (HUY) {
+//   HUY.addEventListener("click", () => {
+//     const uploadImageInput = document.querySelector("[upload-image-input]");
+//     const uploadImagePreview = document.querySelector("[upload-image-preview]");
+//     if (uploadImageInput && uploadImagePreview) {
+//       uploadImageInput.value = "";
+//       uploadImagePreview.src = "";
+//     }
+//   });
+// }
+//end upload audio
+
