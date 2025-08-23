d6d71142c05d7c0a57bb6599bc5f7bed9b3168a9
buiminhdien <dienbui2005@gmail.com>
2025-08-23 13:05:47 +0700

    thêm phần upload audio

--- middlerwares/admin/uploadCloud.middleware.ts
+++ middlerwares/admin/uploadCloud.middleware.ts
@@ -1,34 +1,7 @@
 import { Request, Response, NextFunction} from "express";
-import { v2 as cloudinary} from "cloudinary";
-import streamifier from "streamifier";
-import dotenv from "dotenv";
-import { error } from "console";
-dotenv.config();
-// cloudinary
-cloudinary.config({
-cloud_name: process.env.CLOUD_NAME,
-api_key: process.env.CLOUD_KEY,
-api_secret: process.env.CLOUD_SECRET,
-});
-// End cloudinary
-const streamUpload = (buffer: any) =>{
-    return new Promise((resolve, reject) => {
-    let stream = cloudinary.uploader.upload_stream({resource_type:'auto'}, (error, result) =>{
-    if(result){
-        resolve(result);
-    }else{
-    reject(error);
-    }
-})
 
+import { uploadToCloudinary } from "../../helper/uploadCloud.middlerWare";
 
-streamifier.createReadStream(buffer).pipe(stream);
-});
-};
-const uploadToCloudinary =  async (buffer: any) =>{
-let result = await streamUpload(buffer);
-return result["url"];
-}
 export const uploadSingle =  async (req: Request, res: Response, next:NextFunction) => {
 try {
 const result = await uploadToCloudinary(req["file"].buffer);
@@ -39,3 +12,22 @@ console.log(error);
     next();
 
     };
+
+export const uploadFields =  async (req: Request, res: Response, next: NextFunction) => {
+
+console.log(req["files"]);
+
+for (const key in req["files"]) {
+    req.body[key] = [];
+    const array = req["files"] [key];
+    for (const item of array) {
+        try {
+        const result = await uploadToCloudinary(item.buffer);
+        req.body[key].push(result);
+        } catch (error) {
+    console.log(error);
+    }
+    }
+    }
+    next();
+};
